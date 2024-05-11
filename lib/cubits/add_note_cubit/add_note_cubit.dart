1dd1247fc1289c049e33e2cca733a1aaf96ca73f
MaiWael25 <maiw8757@gmail.com>
2024-05-11 19:48:11 +0300

    create fetch note method

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -8,16 +8,15 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModel note ) async
-  {
+  addNote(NoteModel note) async {
     emit(AddNoteLoading());
     try {
-  var notesBox = Hive.box<NoteModel>(kNotesBox);
-  
-  await notesBox.add(note);
-  emit(AddNoteSucess());
-}   catch (e) {
-     emit(AddNoteFailure(e.toString()));
-}
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+      await notesBox.add(note);
+      emit(AddNoteSucess());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
   }
 }

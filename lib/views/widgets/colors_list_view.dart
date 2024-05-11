5d9cdea5ab5909470be5a4936c9872b655960a23
MaiWael25 <maiw8757@gmail.com>
2024-05-11 23:59:45 +0300

    colors list view part two

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,29 +1,68 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive ;
+  final Color color ;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
+    return isActive? CircleAvatar(
+      backgroundColor: Colors.white,
       radius: 38,
-      backgroundColor: Colors.blue,
+      child:  CircleAvatar(
+        radius: 34,
+        backgroundColor: color,
+      ),
+    )  :  CircleAvatar(
+      radius: 38,
+      backgroundColor: color,
     );
   }
 }
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors =
+  [
+    Color(0xffCDB4DB),
+    Color(0xffFFC8DD),
+    Color(0xffFFAFCC),
+    Color(0xffBDE0FE),
+    Color(0xffA2D2FF),
+
+  ];
   @override
   Widget build(BuildContext context) {
     return  SizedBox(
       height: 38*2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context , index)
       {
-        return ColorItem();
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 6),
+          child: GestureDetector
+          (
+            onTap: (){
+              currentIndex = index;
+              setState(() {
+                
+              });
+            },
+            child: ColorItem(
+              color: colors[index],
+              isActive: currentIndex==index,
+            ),
+          ),
+        );
       }),
     );
   }

65bf8833a3707def5f34fb70e06064161a4a46bf
MaiWael25 <maiw8757@gmail.com>
2024-05-11 18:06:35 +0300

    change add note loadin

--- lib/views/widgets/custom_bottom.dart
+++ lib/views/widgets/custom_bottom.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 
 class CustomButtom extends StatelessWidget {
-  const CustomButtom({super.key, this.onTap});
-  final Function()? onTap ;
+  const CustomButtom({super.key, this.onTap, this.isLoading = false});
+  final Function()? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +15,22 @@ class CustomButtom extends StatelessWidget {
         height: 55,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(8)
-        ),
-        child: const Center(child: Text('Add',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),)),
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+        child: Center(
+            child: isLoading
+                ? const SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ))
+                : const Text(
+                    'Add',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )),
       ),
     );
   }

a043fcf3952eda6bfc29efc4741acd4cae52a8b4
MaiWael25 <maiw8757@gmail.com>
2024-05-11 22:08:51 +0300

    delete note

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -34,7 +34,9 @@ class NoteItem extends StatelessWidget {
                         color: Colors.black.withOpacity(.5), fontSize: 18)),
               ),
               trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(
                     Icons.delete,
                     color: Colors.black,

d27094f0f13851787bbe3144b87391dde0a4559a
MaiWael25 <maiw8757@gmail.com>
2024-05-12 01:00:34 +0300

    add color list to edit view note

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/views/widgets/constants.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
-  final bool isActive ;
-  final Color color ;
+  final bool isActive;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return isActive? CircleAvatar(
-      backgroundColor: Colors.white,
-      radius: 38,
-      child:  CircleAvatar(
-        radius: 34,
-        backgroundColor: color,
-      ),
-    )  :  CircleAvatar(
-      radius: 38,
-      backgroundColor: color,
-    );
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 38,
+            child: CircleAvatar(
+              radius: 34,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 38,
+            backgroundColor: color,
+          );
   }
 }
+
 class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
@@ -29,41 +35,30 @@ class ColorsListView extends StatefulWidget {
 
 class _ColorsListViewState extends State<ColorsListView> {
   int currentIndex = 0;
-  List<Color> colors =
-  [
-    Color(0xffCDB4DB),
-    Color(0xffFFC8DD),
-    Color(0xffFFAFCC),
-    Color(0xffBDE0FE),
-    Color(0xffA2D2FF),
-
-  ];
+  
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: 38*2,
+    return SizedBox(
+      height: 38 * 2,
       child: ListView.builder(
-        itemCount: colors.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context , index)
-      {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 6),
-          child: GestureDetector
-          (
-            onTap: (){
-              currentIndex = index;
-              setState(() {
-                
-              });
-            },
-            child: ColorItem(
-              color: colors[index],
-              isActive: currentIndex==index,
-            ),
-          ),
-        );
-      }),
+          itemCount: kColors.length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: GestureDetector(
+                onTap: () {
+                  currentIndex = index;
+                  BlocProvider.of<AddNoteCubit>(context).color = kColors[index];
+                  setState(() {});
+                },
+                child: ColorItem(
+                  color: kColors[index],
+                  isActive: currentIndex == index,
+                ),
+              ),
+            );
+          }),
     );
   }
 }
--- lib/views/widgets/constants.dart
+++ lib/views/widgets/constants.dart
@@ -3,3 +3,10 @@ import 'package:flutter/animation.dart';
 
 const kPrimaryColor = const Color(0xff62FCD7);
 const  String kNotesBox = 'notes_box';
+List<Color> kColors =const [
+    Color(0xffCDB4DB),
+    Color(0xffFFC8DD),
+    Color(0xffFFAFCC),
+    Color(0xffBDE0FE),
+    Color(0xffA2D2FF),
+  ];
